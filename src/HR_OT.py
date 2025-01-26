0c88bfd78f7ac8ea9cc89ede70bc60e9b35e084c
Peter L. Halmos <ph3641@princeton.edu>
2025-01-26 16:05:20 -0500

    added GW for connectome

--- src/HR_OT.py
+++ src/HR_OT.py
@@ -43,7 +43,9 @@ class HierarchicalRefinementOT:
                  clustering_type: str = 'soft',
                  plot_clusterings: bool = False,
                  parallel: bool = False,
-                 num_processes: Union[int, None] = None
+                 num_processes: Union[int, None] = None,
+                 A = None,
+                 B = None, alpha=0.0
                 ):
     
         self.C = C.to(device)
@@ -55,12 +57,14 @@ class HierarchicalRefinementOT:
         self.plot_clusterings =  plot_clusterings
         self.parallel = parallel
         self.num_processes = num_processes
-
+        self.A, self.B = A.to(device), B.to(device)
+        
         # Point clouds optional attributes
         self.X, self.Y = None, None
         self.N = C.shape[0]
         self.Monge_clusters = None
-
+        self.Quadratic = True if (A is not None or B is not None) else False
+        self.alpha = alpha
         
         
         assert C.shape[0] == C.shape[1], "Currently assume square costs so that |X| = |Y| = N"
@@ -77,7 +81,8 @@ class HierarchicalRefinementOT:
                             clustering_type: str = 'soft',
                             plot_clusterings: bool = False,
                             parallel: bool = False,
-                            num_processes: Union[int, None] = None):
+                            num_processes: Union[int, None] = None,
+                              Quadratic=False, alpha=0.0):
         r"""
         Constructor for initializing from point clouds.
         
@@ -115,6 +120,8 @@ class HierarchicalRefinementOT:
         # Cost-mat an optional attribute
         obj.C = None
         obj.Monge_clusters = None
+        obj.Quadratic = Quadratic
+        obj.alpha = alpha
         
         assert X.shape[0] == Y.shape[0], "Currently assume square costs so that |X| = |Y| = N"
         
@@ -242,18 +249,9 @@ class HierarchicalRefinementOT:
         """
         _x0, _x1 = torch.index_select(self.X, 0, idxX), torch.index_select(self.Y, 0, idxY)
         print(f'x0 shape: {_x0.shape}, x1 shape: {_x1.shape}, rankD: {rankD}')
+        
         if rankD < _x0.shape[0]:
-            C1, C2 = util.low_rank_distance_factorization(_x0,
-                                                          _x1,
-                                                          r=rankD,
-                                                          eps=eps,
-                                                          device=self.device)
-            # Normalize appropriately
-            c = ( C1.max()**1/2 ) * ( C2.max()**1/2 )
-            C1, C2 = C1/c, C2/c
-            C_factors = (C1, C2)
-            A_factors = None
-            B_factors = None
+            C_factors, A_factors, B_factors = get_dist_mats(_x0, _x1, rankD, eps, Quadratic=self.Quadratic )
             # Solve a low-rank OT sub-problem with black-box solver
             Q, R, diagG, errs = self.solver(C_factors, A_factors, B_factors,
                                        gamma=30,
@@ -265,11 +263,17 @@ class HierarchicalRefinementOT:
                                        max_inneriters_relaxed=40,
                                        diagonalize_return=True,
                                        printCost=False, tau_in=100000,
-                                           dtype = C1.dtype)
+                                        dtype = C1.dtype, alpha=self.alpha)
         
         else:
+            # Rank = shape; can compute cost in full and run standard LR-OT
+            if self.Quadratic:
+                A_XY, B_XY = torch.cdist(_x0, _x0), torch.cdist(_x1, _x1)
+            else:
+                A_XY, B_XY = None, None
+            
             C_XY = torch.cdist(_x0, _x1)
-            Q, R, diagG, errs = FRLC_opt(C_XY,
+            Q, R, diagG, errs = FRLC_opt(C_XY, A=A_XY, B=B_XY,
                                    gamma=30,
                                    r = rank_level,
                                    max_iter=50,
@@ -279,7 +283,7 @@ class HierarchicalRefinementOT:
                                    max_inneriters_relaxed=40,
                                    diagonalize_return=True,
                                    printCost=False, tau_in=100000,
-                                       dtype = C_XY.dtype)
+                                       dtype = C_XY.dtype, alpha=self.alpha)
         return Q, R
         
     def _solve_prob(self, idxX, idxY, rank_level):
@@ -291,8 +295,15 @@ class HierarchicalRefinementOT:
         submat = torch.index_select(self.C, 0, idxX)
         C_XY = torch.index_select(submat, 1, idxY)
         
+        if self.A is not None and self.B is not None:
+            # Case: costs given as input
+            A_XY = torch.index_select(torch.index_select(self.A, 0, idxX), 1, idxX)
+            B_XY = torch.index_select(torch.index_select(self.B, 0, idxY), 1, idxY)
+        else:
+            A_XY, B_XY = None, None
+        
         # Solve a low-rank OT sub-problem with black-box solver
-        Q, R, diagG, errs = self.solver(C_XY,
+        Q, R, diagG, errs = self.solver(C_XY, A=A_XY, B=B_XY,
                                    gamma=30,
                                    r = rank_level,
                                    max_iter=50,
@@ -302,13 +313,13 @@ class HierarchicalRefinementOT:
                                    max_inneriters_relaxed=40,
                                    diagonalize_return=True,
                                    printCost=False, tau_in=100000,
-                                       dtype = C_XY.dtype)
+                                       dtype = C_XY.dtype, alpha=self.alpha)
         return Q, R
-
+    
     def _compute_coupling_from_Ft(self):
-        '''
+        """
         Returns coupling as a full-rank matrix rather than as a set of (x, T(x)) pairs.
-        '''
+        """
         size = (self.N, self.N)
         P = torch.zeros(size)
         for pair in self.Monge_clusters:
@@ -317,6 +328,9 @@ class HierarchicalRefinementOT:
         return P / self.N
     
     def compute_OT_cost(self):
+        """
+        Compute the optimal transport in linear space and time (w/o coupling).
+        """
         cost = 0
         for clus in self.Monge_clusters:
             idx1, idx2 = clus
@@ -327,194 +341,28 @@ class HierarchicalRefinementOT:
                 cost += torch.norm(self.X[idx1,:] - self.Y[idx2,:])
         cost = cost / self.N
         return cost
-                
 
-
-
-"""
-
-Below: parallelized version (not object oriented for now)
-
-"""
-
-
-def HROT_worker_subproblem(args):
     
-    C_XY, idxX, idxY, solver, i, rank_level, rank_schedule, base_rank, clustering_type, plot_clusterings, N = args
-    #C_XY = global_cost_matrix[idxX][:, idxY]
-    
-    # Parallelization not GPU optimized currently (but can be, of course)
-    device='cpu'
-    
-    if len(idxX) <= base_rank or len(idxY) <= base_rank:
-                # Return tuple of base-rank sized index sets (e.g. (x,T(x)) for base_rank=1)
-                return [ ( idxX, idxY ) ]
-    
-    try:
-        # Solve a low-rank OT sub-problem with black-box solver
-        Q, R, diagG, errs = FRLC_opt(C_XY,
-                                   gamma=30,
-                                   r = rank_level,
-                                   max_iter= 120,
-                                   device=device,
-                                   min_iter = 40, 
-                                   max_inneriters_balanced=100, 
-                                   max_inneriters_relaxed=100,
-                                   diagonalize_return=True,
-                                   printCost=False, tau_in=100000)
-        print(torch.diag(diagG))
+    def get_dist_mats(_x0, _x1, rankD, eps , Quadratic ):
         
-    except Exception as e:
-        print(f'Solver failed with error: {e}')
-    
-    if plot_clusterings:
-        plt.imshow(Q.cpu().numpy())
-        plt.show()
-        
-        plt.imshow(R.cpu().numpy())
-        plt.show()
-        
-        # to print inner-marginal and verify it is balanced, may print: torch.diag(diagG) 
-    
-    # TODO: case where C_XY itself is low-rank
-    
-    # Compute cluster-assignments for next level
-    # Q: Shape ( len(idxX) x r_level )
-    # R: Shape ( len(idxY) x r_level )
-
-    # Next level cluster capacity
-    capacity = int( N / torch.prod(rank_schedule[0:i+1]) )
-
-    new_coclusters = []
-    
-    if clustering_type == 'soft':
-        
-        idx_seenX = torch.arange(Q.shape[0]).to(device)
-        idx_seenY = torch.arange(R.shape[0]).to(device)
-        
-        for z in range(rank_level):
+        if Quadratic:
+            A_factors = ret_normalized_cost(_x0, _x0, rankD, eps)
+            B_factors = ret_normalized_cost(_x1, _x1, rankD, eps)
+        else:
+            A_factors = None
+            B_factors = None
             
-            # Greedy assignment : ensure one _exactly_ fills the capacity of each cluster
-            topk_values, topk_indices_X = torch.topk( Q[idx_seenX][:,z], k=capacity )
-            idxX_z = idxX[idx_seenX[topk_indices_X]]
-            topk_values, topk_indices_Y = torch.topk( R[idx_seenY][:,z], k=capacity )
-            idxY_z = idxY[idx_seenY[topk_indices_Y]]
-            
-            new_coclusters.append(( idxX_z, idxY_z ))
-            
-            #print(f'Length of assignment sets X: {len(topk_indices_X)}, Y: {len(topk_indices_Y)} [=capacity]')
-            idx_seenX = idx_seenX[~torch.isin(idx_seenX, idx_seenX[topk_indices_X])]
-            idx_seenY = idx_seenY[~torch.isin(idx_seenY, idx_seenY[topk_indices_Y])]
+        C_factors = ret_normalized_cost(_x0, _x1, rankD, eps)
+        return C_factors, A_factors, B_factors
     
-    else:
-        zX = torch.argmax(Q, axis=1) # X-assignments
-        zY = torch.argmax(R, axis=1) # Y-assignments
-        
-        for z in range(rank_level):
-            
-            idxX_z = idxX[zX == z]
-            idxY_z = idxY[zY == z]
-
-            assert len(idxX_z) == len(idxY_z) == capacity, \
-                                "Assertion failed! Not a hard-clustering function, or point sets of unequal size!"
-            
-            new_coclusters.append(( idxX_z, idxY_z ))
-
-    return new_coclusters
-
-
-def hierarchical_refinement_parallelized(C, rank_schedule, solver, base_rank=1, \
-                            clustering_type='soft', plot_clusterings=False, return_as_coupling=False, \
-                                         num_processes=None):
-    '''
-    A wrapper for solving HR-OT using node parallelization (cpu)
-    
-    Parameters
-    ----------
-    X: (n,d) torch.tensor
-        First dataset of n points in R^d
-    Y: (n,d) torch.tensor
-        Second dataset of n points in R^d
-    rank_schedule: list
-        List of ranks to use at each level of hierarchical refinement
-        which factorizes n:
-                    rank_schedule[0]*...*rank_schedule[-1] = n
-    solver: function
-        Black-box solver for low-rank OT instance
-    base_rank: int (default: 1)
-        Base-case rank at which to output minimal set of tuples (x, T(x))
-    clustering_type: str
-        If the low-rank OT returns an approximate "soft" clustering, 
-        forces it to be hard one for cluster sizes to remain Monge-consistent.
-        Otherwise, takes arg-max.
-    plot_clusterings: bool
-        If true, plots a heatmap of the Q and R clusterings.
-    return_as_coupling: bool
-        If true, returns a coupling matrix. Else, returns a sparse list of points with their Monge mapping.
-    num_processes: int
-        The number of nodes (CPUs) on which to run.
-    
-     Returns
-    -------
-    gamma : torch.tensor of shape (N, N)
-        Optimal transport plan.
-    (OR)
-    F_t : list (size N)
-        List of (x, T(x)) tuples
-    '''
-    
-    # Assuming strictly CPU Parallelization (todo: extend to gpu)
-    device = 'cpu'
-    
-    mp.set_start_method("spawn", force=True)
-    C.share_memory_()
-    
-    F_t = [(torch.arange(C.shape[0]).to(device), torch.arange(C.shape[1]).to(device))]
-    N = C.shape[0]
-    
-    if num_processes is None:
-        num_processes = mp.cpu_count()
-    
-    with mp.Pool(processes = num_processes) as pool: # , initializer=init_worker_process, initargs=(C,)
-        
-        for i, rank_level in enumerate(rank_schedule):
-            # Iterate over ranks in the scheduler
-            F_tp1 = []
-    
-            subproblem_args = []
-            # Building args as input for mp
-            for (idxX, idxY) in F_t:
-                
-                submat = torch.index_select(C, 0, idxX)
-                C_XY = torch.index_select(submat, 1, idxY)
-                subproblem_args.append(
-                    ( C_XY, 
-                     idxX, 
-                     idxY, 
-                     solver, 
-                     i, 
-                     rank_level, 
-                     rank_schedule, 
-                     base_rank, 
-                     clustering_type, 
-                     plot_clusterings, 
-                     N )
-                )
-            
-            print(f'Mapping subproblems')
-            
-            level_results = pool.map( HROT_worker_subproblem, subproblem_args )
-            
-            for co_cluster_lst in level_results:
-                F_tp1.extend(co_cluster_lst)
-            
-            F_t = F_tp1
-
-    if return_as_coupling is False:
-        # Returns set of x,T(x) tuples
-        return F_t
-
-    else:
-        # Returns a sparse coupling
-        return compute_coupling_from_Ft(F_t, C.shape)
-
+    def ret_normalized_cost(X, Y, rankD, eps):
+        C1, C2 = util.low_rank_distance_factorization(X,
+                                                      Y,
+                                                      r=rankD,
+                                                      eps=eps,
+                                                      device=self.device)
+        # Normalize appropriately
+        c = ( C1.max()**1/2 ) * ( C2.max()**1/2 )
+        C1, C2 = C1/c, C2/c
+        C_factors = (C1, C2)
+        return C_factors
